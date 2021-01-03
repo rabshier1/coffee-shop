914e43b3ce6ecbe6ba0bb698d0cf7c8c5127f1f9
Rodney Abshier <rabshier1@hotmail.com>
2021-01-03 14:52:29 -0600

    Add blog post pages

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,14 +4,16 @@ import BlogPost from './BlogPost';
 export default function BlogList() {
   const data = useStaticQuery(graphql`
     {
-      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC })
-	{
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
             id
             frontmatter {
               title
               date(formatString: "MMMM D, YYYY")
+            }
+            fields {
+              slug
             }
             excerpt
           }
@@ -24,9 +26,11 @@ export default function BlogList() {
       {data.allMarkdownRemark.edges.map(edge => (
         <BlogPost
           key={edge.node.id}
+          slug={edge.node.fields.slug}
           title={edge.node.frontmatter.title}
           date={edge.node.frontmatter.date}
-          excerpt={edge.node.excerpt} />
+          excerpt={edge.node.excerpt}
+        />
       ))}
     </div>
   );
