cbe759b2628e11de26ffecc15e02a4f8095636dd
Rodney Abshier <rabshier1@hotmail.com>
2021-01-03 17:07:14 -0600

    Add blog post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const { createFilePath } = require('gatsby-source-filesystem');
 exports.onCreateNode = function({ node, getNode, actions }) {
   const { createNodeField } = actions;
@@ -12,7 +11,6 @@ exports.onCreateNode = function({ node, getNode, actions }) {
     });
   }
 };
-
 exports.createPages = async function({ graphql, actions }) {
   const { createPage } = actions;
   const result = await graphql(`
@@ -39,4 +37,24 @@ exports.createPages = async function({ graphql, actions }) {
         }
       });
     });
+    const posts = result.data.allMarkdownRemark.edges;
+    const pageSize = 5;
+    const pageCount = Math.ceil(posts.length / pageSize);
+    const templatePath = path.resolve('src/templates/blog-list.js');
+    for (let i = 0; i < pageCount; i++) {
+      let path = '/blog';
+      if (i > 0) {
+        path += `/${i + 1}`;
+      }
+      createPage({
+        path,
+        component: templatePath,
+        context: {
+          limit: pageSize,
+          skip: i * pageSize,
+          pageCount,
+          currentPage: i + 1
+        }
+      });
+    }
 };
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import BlogPost from './BlogPost';
 export default function BlogList() {
   const data = useStaticQuery(graphql`
     {
-      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      allMarkdownRemark(
+        sort: { fields: frontmatter___date, order: DESC }
+        limit: 3
+        ) {
         edges {
           node {
             id
@@ -32,6 +35,9 @@ export default function BlogList() {
           excerpt={edge.node.excerpt}
         />
       ))}
+      <div>
+        <Link to="/blog">More >></Link>
+      </div>
     </div>
   );
 }
