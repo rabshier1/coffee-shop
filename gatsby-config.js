f6ed4fc9bb578beb8ba7d72fc689f4e70abaec82
Rodney Abshier <rabshier1@hotmail.com>
2021-01-09 08:26:17 -0600

    cms attempt in index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,22 @@ module.exports = {
   siteMetadata: {
     title: 'The Coffee Blog'
   },
-  
   plugins: [
     'gatsby-plugin-netlify-cms',
-	{
-		resolve: 'gatsby-source-filesystem',
-		options : {
-			name: 'blog',
-			path: 'src/blog'
-		}
-	},
-	'gatsby-transformer-remark'
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: 'src/blog'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pageData',
+        path: 'src/pageData'
+      }
+    },
+    'gatsby-transformer-remark'
   ]
 };
